46e47bafbd5c77344a415b2f04c8e939b1d9d298
Jose Davalos Rosas <jdavros@gmail.com>
2022-05-08 11:54:10 -0700

    Refactored first Networking API to include dependency injection for easier testing

--- Sources/DemoFramework/Networking.swift
+++ Sources/DemoFramework/Networking.swift
@@ -7,21 +7,37 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+}
+
 extension DemoFramework {
     public class Networking {
         public class Manager {
             /// Responsible for handling all networking calls
             /// - Warning: Must create before using any public APIs
-            private let session = URLSession.shared
+            internal var session: NetworkSession = URLSession.shared
             
             public init() {}
             
+            /// Calls live internet to retrieve Data from a specific location
+            /// - Parameters:
+            ///   - url: The location you want to retrieve data from
+            ///   - completionHandler: Returns a result object which signifies the status of the request
             public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
-                let task = session.dataTask(with: url) { data, response, error in
+                session.get(from: url) { data, error in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completionHandler(result)
                 }
-                task.resume()
             }
             
             public enum NetworkResult<Value> {
--- Tests/DemoFrameworkTests/DemoFrameworkNetworkingTests.swift
+++ Tests/DemoFrameworkTests/DemoFrameworkNetworkingTests.swift
@@ -12,22 +12,35 @@ final class DemoFrameworkNetworkingTests: XCTestCase {
     func test_loadDataCall() {
         // Arrange
         let manager = DemoFramework.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
         let expectation = expectation(description: "Called for data")
-        guard let url = URL(string: "https://google.com") else {
-            return XCTFail("Could not create URL properly")
-        }
+        let data = Data([0, 1, 0, 1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
         
         // Act & Assert
         manager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
             case let .success(returnedData):
-                XCTAssertNotNil(returnedData, "Response data is nil")
+                XCTAssertEqual(data, returnedData, "manager returned unexpected data")
             case let .failure(error):
                 XCTFail(error?.localizedDescription ?? "error forming error result")
             }
         }
         
         wait(for: [expectation], timeout: 1.0)
+    }
+    
+    class NetworkSessionMock: NetworkSession {
+        var data: Data?
+        var error: Error?
+        
+        func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+            completionHandler(data, error)
+        }
+        
+        
     }
 }
