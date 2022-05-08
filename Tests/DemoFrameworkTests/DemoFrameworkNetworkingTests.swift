db66c798ea30da00d92a305ca5ebbaf86b0cccdc
Jose Davalos Rosas <jdavros@gmail.com>
2022-05-08 14:35:18 -0700

    Added a second Networking API for sending data

--- Tests/DemoFrameworkTests/DemoFrameworkNetworkingTests.swift
+++ Tests/DemoFrameworkTests/DemoFrameworkNetworkingTests.swift
@@ -21,16 +21,46 @@ final class DemoFrameworkNetworkingTests: XCTestCase {
         
         // Act & Assert
         manager.loadData(from: url) { result in
-            expectation.fulfill()
             switch result {
             case let .success(returnedData):
                 XCTAssertEqual(data, returnedData, "manager returned unexpected data")
             case let .failure(error):
                 XCTFail(error?.localizedDescription ?? "error forming error result")
             }
+            expectation.fulfill()
         }
         
         wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func test_sendDataCall() {
+        // Arrange
+        let session = NetworkSessionMock()
+        let manager = DemoFramework.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "Jose")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expectation = expectation(description: "Wait for send data to be completed.")
+        
+        manager.sendData(to: url, body: sampleObject) { result in
+            switch result {
+            case let .success(returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, sampleObject)
+            case let .failure(error):
+                XCTFail("Expected success but got \(String(describing: error)) instead.")
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    struct MockData: Codable, Equatable {
+        var id: Int
+        var name: String
     }
     
     class NetworkSessionMock: NetworkSession {
@@ -41,6 +71,8 @@ final class DemoFrameworkNetworkingTests: XCTestCase {
             completionHandler(data, error)
         }
         
-        
+        func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+            completionHandler(data, error)
+        }
     }
 }
